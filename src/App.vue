c163a240f02dd637df6b5f4d0e94d28925128a2b
dmancilla85 <david.a.m@live.com>
2020-04-21 16:51:45 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'event-list' }">List</router-link> |
+      <router-link :to="{ name: 'event-create' }">Create</router-link> |
+      <router-link :to="{ name: 'user', params: { username: 'Gregg' } }"
+        >User</router-link
+      >
     </div>
     <router-view />
   </div>
@@ -10,13 +13,12 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
